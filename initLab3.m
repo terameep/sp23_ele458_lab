02c19ee0038bc05b7863c1e511524cb1a8036dbd
terameep <124088983+terameep@users.noreply.github.com>
2023-03-22 15:10:25 -0400

    lab3 simulations 2.1 done

--- initLab3.m
+++ initLab3.m
@@ -1,43 +1,30 @@
 load("sroots.mat");
 
 %% initial params
-alpha = 25;
-beta = 2334;
 
-constA = 41.5;
-constC = alpha;
-constD = beta;
-n = 495;
-g = 9.81;
+x0 = [0; 0; 0; 0.2];
 
-x0 = [0.17; 0; 0; 0];
-noisePower = 0.002;
-T = 0.01;
-
-A = [0, 1, 0, 0;
-    constA, 0, 0, -(constA .* constC)./(n .* g);
+A = [0, 0, 1, 0;
     0, 0, 0, 1;
-    0, 0, 0, -constC];
-B = [0; (constA .* constD)./(n .* g); 0; constD];
+    -1.7117, 0, -0.3249, 0;
+    0, 0, 0, -1.0004];
+B = [0, 0;
+    0, 0;
+    0.0377, 0.0959;
+    -0.1228, 0.1];
 C = eye(4);
-D = zeros(4,1);
+D = zeros(4,2);
 
-%% slider variables
-Ts = .66; % default 1, between .1 and 2
-f = 5; % greater than or equal default 5
+Ts = 2;
 
-%% filter
-Tsf = Ts/f; % filter settling time; choose f to be greater than or equal to 5
-bwfp = (s1+1j*s1)/Tsf; % 2nd-order Butterworth filter pole scaled to Tsf-sec settling time
-den = poly([bwfp conj(bwfp)]);
-num = [den(end) 0]; % numerator is the constant term in the denominator polynomial
-vfilter_a = tf(num,den); % analog differentiator with lowpass filter
-vfilter_d = c2d(vfilter_a,T,'tustin'); % digital differentiator with lowpass filter
-[Af,Bf,Cf,Df]=ssdata(vfilter_d); % convert vfilter_d to state-space model
 
 %% poles
-sPoles = s4/Ts;
-% sPoles = [-25 s3/Ts]
+imag2ADP = [1.2982j];
+ADP = [s1/Ts + imag2ADP(1,1)];
+sPoles = [ADP(1,1), conj(ADP(1,1)), s2/Ts]
+
+n = size(A,1); %size should be 4
+T = min(Ts ./ (20 .* n), pi ./ (5 .* 1.2982)) %0.025
 
 zpoles = exp(T * sPoles);
 
@@ -45,4 +32,7 @@ zpoles = exp(T * sPoles);
 
 K = place(phi, gamma, zpoles);
 
-dsm(phi, gamma, K)
+[delta1, delta2] = rb_regsf(phi, gamma, K, T)
+if(~(min(delta1, delta2) >= 0.5))
+    disp('warning: stability margin below 0.5')
+end
